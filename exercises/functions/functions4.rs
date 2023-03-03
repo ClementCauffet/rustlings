fe82c5f3a0568087a511ef262eecf14dca6b01be
ClementCauffet <ccauffet@tech.aubay.com>
2023-03-03 09:30:42 +0100

    test

--- exercises/functions/functions4.rs
+++ exercises/functions/functions4.rs
@@ -1,7 +1,7 @@
 // functions4.rs
 // Execute `rustlings hint functions4` or use the `hint` watch subcommand for a hint.
 
-// This store is having a sale where if the price is an even number, you get
+// This store is having a sale where if the price is an even number, you g
 // 10 Rustbucks off, but if it's an odd number, it's 3 Rustbucks off.
 // (Don't worry about the function bodies themselves, we're only interested
 // in the signatures for now. If anything, this is a good way to peek ahead
