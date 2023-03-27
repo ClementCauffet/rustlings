b2d00ddcd2aeab8180be8b4ddf8dcd32201ecb90
ClementCauffet <ccauffet@tech.aubay.com>
2023-03-27 16:44:34 +0200

    47/94

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,32 +14,49 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
+
 struct Team {
     name: String,
     goals_scored: u8,
     goals_conceded: u8,
 }
 
-fn build_scores_table(results: String) -> HashMap<String, Team> {
+fn build_scores_table(results: &str) -> HashMap<String, Team> {
     // The name of the team is the key and its associated struct is the value.
     let mut scores: HashMap<String, Team> = HashMap::new();
 
     for r in results.lines() {
-        let v: Vec<&str> = r.split(',').collect();
-        let team_1_name = v[0].to_string();
-        let team_1_score: u8 = v[2].parse().unwrap();
-        let team_2_name = v[1].to_string();
-        let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be the number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+        let mut parts = r.split(',');
+        let team_1_name = parts.next().unwrap().to_string();
+        let team_2_name = parts.next().unwrap().to_string();
+        let team_1_score: u8 = parts.next().unwrap().parse().unwrap();
+        let team_2_score: u8 = parts.next().unwrap().parse().unwrap();
+
+        scores
+            .entry(team_1_name.clone())
+            .and_modify(|t| {
+                t.goals_scored += team_1_score;
+                t.goals_conceded += team_2_score;
+            })
+            .or_insert(Team {
+                name: team_1_name,
+                goals_scored: team_1_score,
+                goals_conceded: team_2_score,
+            });
+        scores
+            .entry(team_2_name.clone())
+            .and_modify(|t| {
+                t.goals_scored += team_2_score;
+                t.goals_conceded += team_1_score;
+            })
+            .or_insert(Team {
+                name: team_2_name,
+                goals_scored: team_2_score,
+                goals_conceded: team_1_score,
+            });
     }
     scores
 }
@@ -59,7 +76,7 @@ mod tests {
 
     #[test]
     fn build_scores() {
-        let scores = build_scores_table(get_results());
+        let scores = build_scores_table(&get_results());
 
         let mut keys: Vec<&String> = scores.keys().collect();
         keys.sort();
@@ -71,7 +88,7 @@ mod tests {
 
     #[test]
     fn validate_team_score_1() {
-        let scores = build_scores_table(get_results());
+        let scores = build_scores_table(&get_results());
         let team = scores.get("England").unwrap();
         assert_eq!(team.goals_scored, 5);
         assert_eq!(team.goals_conceded, 4);
@@ -79,7 +96,7 @@ mod tests {
 
     #[test]
     fn validate_team_score_2() {
-        let scores = build_scores_table(get_results());
+        let scores = build_scores_table(&get_results());
         let team = scores.get("Spain").unwrap();
         assert_eq!(team.goals_scored, 0);
         assert_eq!(team.goals_conceded, 2);
